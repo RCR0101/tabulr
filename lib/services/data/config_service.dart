3f6208abd8d7991aa513d73fc33a177453b49e36
RCR0101 <dalmia.aryan@gmail.com>
2026-05-28 09:44:48 +0530

    chore: version update

--- lib/services/data/config_service.dart
+++ lib/services/data/config_service.dart
@@ -10,7 +10,7 @@ class ConfigService {
   // App Configuration
   String get appName => 'Tabulr';
   // Keep in sync with pubspec.yaml `version:` field.
-  String get appVersion => '1.10.9';
+  String get appVersion => '1.10.10';
 
   // Debug Settings
   bool get debugMode => false;
