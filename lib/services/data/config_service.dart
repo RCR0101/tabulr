d5dc5cb7ec256bae385ae9cdf8425637103c6bd0
RCR0101 <dalmia.aryan@gmail.com>
2026-07-30 22:57:59 +0530

    chore: legacy

--- lib/services/data/config_service.dart
+++ lib/services/data/config_service.dart
@@ -16,7 +16,7 @@ class ConfigService {
   // App Configuration
   String get appName => 'Tabulr';
   // Keep in sync with pubspec.yaml `version:` field.
-  String get appVersion => '2.5.57';
+  String get appVersion => '2.5.58';
 
   // Debug Settings
   bool get debugMode => false;
