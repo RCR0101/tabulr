37935fc58ee4cd2ef7115774bacfbdf8a05cc7f3
RCR0101 <dalmia.aryan@gmail.com>
2026-08-02 16:01:11 +0530

    feat: better loading screen

--- lib/services/data/config_service.dart
+++ lib/services/data/config_service.dart
@@ -16,7 +16,7 @@ class ConfigService {
   // App Configuration
   String get appName => 'Tabulr';
   // Keep in sync with pubspec.yaml `version:` field.
-  String get appVersion => '2.5.65';
+  String get appVersion => '2.5.66';
 
   // Debug Settings
   bool get debugMode => false;
